9376c6c928c4e24601e8a14074b1af12797a6ad9
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-04-23 16:41:20 +0200

    expand LoginManager tests

--- ue4/src/test/java/teaching/swe/streaming/LoginManagerTest.java
+++ ue4/src/test/java/teaching/swe/streaming/LoginManagerTest.java
@@ -5,17 +5,29 @@ import org.junit.jupiter.api.Test;
 
 public class LoginManagerTest {
     @Test
-    public void testCheckAuth() {
+    public void testCheckAuthTrue() {
         LoginManager lm = new LoginManager();
 
         Assertions.assertTrue(lm.checkAuth("User1", "PasswortA"));
         Assertions.assertTrue(lm.checkAuth("User2", "PasswortB"));
         Assertions.assertTrue(lm.checkAuth("User3", "PasswortC"));
         Assertions.assertTrue(lm.checkAuth("User4", "PasswortC"));
+    }
+
+    @Test
+    public void testCheckAuthFalse() {
+        LoginManager lm = new LoginManager();
 
         Assertions.assertFalse(lm.checkAuth("User", "PasswortA"));
         Assertions.assertFalse(lm.checkAuth("User2", "PasswortD"));
         Assertions.assertFalse(lm.checkAuth("", "PasswortC"));
         Assertions.assertFalse(lm.checkAuth("\t\n#124124", "PasswortC"));
+    }
+
+    @Test
+    public void testCheckAuthNull() {
+        LoginManager lm = new LoginManager();
+
+        Assertions.assertFalse(lm.checkAuth(null, null));
     }
 }
