42cb29787aaac1b9370bc4bf8e5b087aa5e19c66
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-04-24 13:19:15 +0200

    StreamingApplicationTest: add login() test

--- ue4/src/test/java/teaching/swe/streaming/StreamingApplicationTest.java
+++ ue4/src/test/java/teaching/swe/streaming/StreamingApplicationTest.java
@@ -1,25 +1,35 @@
 package teaching.swe.streaming;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import teaching.swe.streaming.fraud.FraudDetectionMock;
+import teaching.swe.streaming.fraud.IFraudDetection;
+import teaching.swe.streaming.recommender.IRecommenderSystem;
+import teaching.swe.streaming.recommender.RecommenderSystem;
+import teaching.swe.streaming.recommender.RecommenderSystemMock;
+
 public class StreamingApplicationTest {
-    
+
     @Test
     public void testSetVolumeWithinRange() {
         int newVolume = 20;
-        
+
         StreamingApplication sa = new StreamingApplication();
-        
+
         int result = sa.setVolume(newVolume);
 
         Assertions.assertEquals(newVolume, result);
         Assertions.assertTrue(result > -1);
         Assertions.assertEquals(newVolume, sa.getVolume());
-        
 
         newVolume = 90;
-        
+
         result = sa.setVolume(newVolume);
 
         Assertions.assertEquals(newVolume, result);
@@ -30,16 +40,15 @@ public class StreamingApplicationTest {
         int newVolume = -5;
         StreamingApplication sa = new StreamingApplication();
         int oldVolume = sa.getVolume();
-        
+
         int result = sa.setVolume(newVolume);
 
         Assertions.assertEquals(-1, result);
         Assertions.assertTrue(result == -1);
         Assertions.assertEquals(oldVolume, sa.getVolume());
 
-
         newVolume = 110;
-        
+
         result = sa.setVolume(newVolume);
 
         Assertions.assertEquals(-1, result);
@@ -50,7 +59,7 @@ public class StreamingApplicationTest {
         int minVolume = 0;
         int maxVolume = 100;
         StreamingApplication sa = new StreamingApplication();
-        
+
         // test allowed edges (test min and max volume)
         Assertions.assertEquals(minVolume, sa.setVolume(minVolume));
         Assertions.assertEquals(minVolume, sa.getVolume());
@@ -59,11 +68,29 @@ public class StreamingApplicationTest {
 
         // reset volume
         sa.setVolume(50);
-        
+
         // test not allowed edges
         Assertions.assertEquals(-1, sa.setVolume(-1));
         Assertions.assertEquals(50, sa.getVolume());
         Assertions.assertEquals(-1, sa.setVolume(101));
         Assertions.assertEquals(50, sa.getVolume());
+    }
+
+    @Test
+    public void testLogin() {
+        IRecommenderSystem rs = new RecommenderSystemMock();
+        IFraudDetection fd = new FraudDetectionMock();
+        ILoginManager lm = new LoginManagerMock(true);
+        StreamingApplication sa = new StreamingApplication(rs, fd, lm);
+
+        LoginRequest request = new LoginRequest();
+        request.setLocation("DE");
+        request.setUserName("Test User");
+        request.setPassword("Test PWD");
+
+        LoginResponse response = sa.login(request);
+
+        assertTrue(response.isSuccessful());
+        assertEquals(Arrays.asList("Dummy Movie A"), response.getRecommendations());
     }
 }
