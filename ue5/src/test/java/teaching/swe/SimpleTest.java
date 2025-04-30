6590a49fe467ebac7cee7982e480e939fcdb23da
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-04-30 14:51:07 +0200

    add ue5

---
+++ ue5/src/test/java/teaching/swe/SimpleTest.java
@@ -0,0 +1,13 @@
+package teaching.swe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+    @Test
+    void test() {
+        assertEquals(2, 1 + 1);
+    }
+}
