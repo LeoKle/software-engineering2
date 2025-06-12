7e4f79da60021ce4df13e5dc8b8ff199bcb5e704
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-06-12 09:44:18 +0200

    add s2 - ue1

---
+++ s2/src/main/java/hsdinflab/swe/State.java
@@ -0,0 +1,49 @@
+package hsdinflab.swe;
+
+public class State {
+    public enum SongState {
+        READY, PLAYING, PAUSED, STOPPED
+    };
+
+    private SongState songState;
+
+    public State() {
+        this.songState = SongState.READY;
+    }
+
+    public SongState getSongState() {
+        return songState;
+    }
+
+    public void play() {
+        if (songState != SongState.PAUSED && songState != SongState.READY) {
+            throw new IllegalStateException("Cannot play when not paused or ready");
+        }
+
+        songState = SongState.PLAYING;
+    }
+
+    public void pause() {
+        if (songState != SongState.PLAYING) {
+            throw new IllegalStateException("Cannot pause when not playing");
+        }
+
+        songState = SongState.PAUSED;
+    }
+
+    public void stop() {
+        if (songState != SongState.PLAYING) {
+            throw new IllegalStateException("Cannot stop when already stopped");
+        }
+
+        songState = SongState.STOPPED;
+    }
+
+    public void foward_backward() {
+        if (songState != SongState.PLAYING && songState != SongState.PAUSED) {
+            throw new IllegalStateException("Cannot forward or backward when not playing or paused");
+        }
+
+        // DONT CHANGE STATE, just simulate the action
+    }
+}
