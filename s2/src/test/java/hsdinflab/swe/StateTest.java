a2d082574492a14e60aca537abeb3a72f7f75fab
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2025-06-23 22:38:10 +0200

    changed StateMock to StateTest

--- s2/src/test/java/hsdinflab/swe/StateTest.java
+++ s2/src/test/java/hsdinflab/swe/StateTest.java
@@ -1,5 +1,6 @@
 package hsdinflab.swe;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,35 @@ public class StateTest {
     public void testGetAndSetState() {
         // Test initial state
         Assertions.assertEquals(State.SongState.READY, state.getSongState());
+    }
+
+    @Test
+    public void testStart() {
+        state.play();
+        State.SongState resultState = state.getSongState();
+        assertEquals(State.SongState.PLAYING, resultState);
+    }
+
+    @Test
+    public void testPause() {
+        state.play();      // bereit → spielt
+        State.SongState resultState = state.getSongState();
+        assertEquals(State.SongState.PLAYING, resultState);
+        state.pause();     // spielt → pausiert
+        State.SongState resultState1 = state.getSongState();
+        assertEquals(State.SongState.PAUSED, resultState1);
+        state.play();      // pausiert → spielt
+        State.SongState resultState2 = state.getSongState();
+        assertEquals(State.SongState.PLAYING, resultState2);
+        state.stop();      // spielt → bereit
+        State.SongState resultState3 = state.getSongState();
+        assertEquals(State.SongState.READY, resultState3);
+    }
+
+    @Test
+    public void testFoward(){
+        state.forward_backward(); 
+        State.SongState resultState = state.getSongState();
+        assertEquals(State.SongState.READY, resultState);
     }
 }
